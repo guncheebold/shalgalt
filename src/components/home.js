ed834ad13bd9e3ab8ebbdfd185885050fc7936d3
guncheebold <guncheebold@gmail.com>
2023-02-04 10:24:58 +0800

    shalgalt

---
+++ src/components/home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./home.css"
+import Zurag1 from "../assets/Zurag 1.jpg"
+const Home = () => {
+    const data = [
+        {
+            key:1,
+            img: Zurag1
+    
+        },
+        {
+            key:1,
+            img: Zurag1
+    
+        },
+        {
+            key:1,
+            img: Zurag1
+    
+        },
+        {
+            key:1,
+            img: Zurag1
+    
+        }
+    ]
+    return (
+        <div className="button">
+            <div className="button1">
+                <h2>readmore</h2>
+            </div>
+            
+        </div>
+    )
+};
+export default Home;
